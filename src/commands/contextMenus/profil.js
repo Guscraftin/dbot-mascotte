80765f035ef51a7bc21b9dd1f655f9d77ca0746b
Guscraftin <97472565+Guscraftin@users.noreply.github.com>
2023-07-23 12:18:08 +0200

    updt: profil - add avatar to user who is not in db

--- src/commands/contextMenus/profil.js
+++ src/commands/contextMenus/profil.js
@@ -10,23 +10,30 @@ module.exports = {
     async execute(interaction) {
         // Get the information about the member
         const user = await Members.findOne({ where: { id: interaction.targetId } });
-        if (!user) return interaction.reply({ content: "Ce membre n'est pas dans la base de données.", ephemeral: true });
-
         const member = await interaction.guild.members.fetch(interaction.targetId);
 
         // Create the embed
-        const fields = [
-            { name: "Date d'anniversaire", value: "23 mars 2023", inline: true },
-        ];
-        if (user.mute_time) fields.push({ name: "Sanction: Exclusion", value: `Se termine <t:${parseInt(user.mute_time / 1000)}:R>`, inline: true }); 
-
         const embed = new EmbedBuilder()
             .setAuthor({ name: `${member.displayName} (${member.id})`, iconURL: member.user.displayAvatarURL() })
             .setColor(colorBasic)
-            .setDescription(`Voici les informations de ce membre :`)
-            .setFields(fields)
+            .setThumbnail(member.user.displayAvatarURL({ dynamic: true }))
             .setTimestamp()
             .setFooter({ text: interaction.guild.name, iconURL: interaction.guild.iconURL() })
+
+        // If the user is in the database (set description and fields)
+        if (user) {
+            embed.setDescription(`Voici les informations de ce membre :`)
+            
+            const fields = [
+                { name: "Date d'anniversaire", value: "23 mars 2023", inline: true },
+            ];
+            if (user.mute_time) fields.push({ name: "Sanction: Exclusion", value: `Se termine <t:${parseInt(user.mute_time / 1000)}:R>`, inline: true });
+            embed.setFields(fields)
+
+        // If the user is not in the database (set description)
+        } else {
+            embed.setDescription(`Ce membre n'est pas enregistré dans la base de données.`)
+        }
 
         return interaction.reply({ embeds: [embed], ephemeral: true})
     }
