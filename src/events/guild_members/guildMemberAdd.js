eef473ac6673ab868a0345b838bcfcb0e5e13894
Guscraftin <97472565+Guscraftin@users.noreply.github.com>
2023-07-23 10:45:29 +0200

    feat: add guildMemberAdd event to add roles

---
+++ src/events/guild_members/guildMemberAdd.js
@@ -0,0 +1,16 @@
+const { Events } = require('discord.js');
+const { roleSeparator, roleStudents } = require(process.env.CONSTANT);
+const { Guilds } = require('../../dbObjects.js');
+
+module.exports = {
+    name: Events.GuildMemberAdd,
+    async execute(member) {
+        // Get information about the guild in the database
+        let guildDB = await Guilds.findOne({ where: { id: member.guild.id } });
+        if (!guildDB) guildDB = await Guilds.create({ id: member.guild.id });
+
+        // Add the role to the member
+        if (guildDB.automatic_verified) await member.roles.add(roleStudents);
+        await member.roles.add(roleSeparator);
+    },
+};
