75d08dcc617e1713cad2ce2c7a2efd358d5be48d
Guscraftin <97472565+Guscraftin@users.noreply.github.com>
2023-08-16 18:45:24 +0200

    feat: delete definitive of a ticket for admin

---
+++ src/buttons/tickets/ticket_delete-admin.js
@@ -0,0 +1,19 @@
+const { ActionRowBuilder, ButtonBuilder, ButtonStyle } = require("discord.js");
+
+module.exports = {
+    data: {
+        name: "ticket_delete-admin",
+    },
+    async execute(interaction) {
+        const deleteTicket = new ActionRowBuilder()
+        .addComponents(
+            new ButtonBuilder()
+              .setCustomId("ticket_delete-admin_confirm")
+              .setLabel("Oui ▸ Supprimer définitivement")
+              .setEmoji("🚨")
+              .setStyle(ButtonStyle.Secondary)
+        );
+
+        return interaction.reply({ content: "## Voulez-vous supprimer définitivement ce ticket ?", components: [deleteTicket], ephemeral: true });
+    }
+}
---
+++ src/buttons/tickets/ticket_delete-admin_confirm.js
@@ -0,0 +1,9 @@
+
+module.exports = {
+    data: {
+        name: "ticket_delete-admin_confirm",
+    },
+    async execute(interaction) {
+        await interaction.channel.delete();
+    }
+}
