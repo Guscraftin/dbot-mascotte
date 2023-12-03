75a86f2acc5c3e216b398f31b05473313a20b9e5
Guscraftin <97472565+Guscraftin@users.noreply.github.com>
2023-12-04 00:47:26 +0100

    updt: add start log when bot start

--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -28,14 +28,19 @@ module.exports = {
         // Check if the bot is synchronized with the server
         const userConnect = await initApp();
         console.log(`1/6 : ${userConnect} is correctly connect to the app !`);
+        console.log(`2/6 : Start initialize mails...`);
         await initCheckMail(guild);
         console.log(`2/6 : Initialize mails checked !`);
+        console.log("3/6 : Start check birthdays...");
         await checkBirthdays(guild);
         console.log("3/6 : Birthdays checked !");
+        console.log("4/6 : Start check user timeout...");
         await muteTimeout(guild);
         console.log("4/6 : Timeout checked !");
+        console.log("5/6 : Start remove empty voice channel...");
         await removeEmptyVoiceChannel(guild);
         console.log("5/6 : Empty voice channel removed !");
+        console.log("6/6 : Start synchronize roles...");
         await syncRoles(guild);
         console.log("6/6 : Roles synchronized !");
 
